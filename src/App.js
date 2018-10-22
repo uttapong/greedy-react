acc8336cf339971f25383f42bdac36916b665f2e
uttapong ruangrit <uttapong@tencent.co.th>
2018-10-22 18:43:34 +0700

    add Denom components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import Denom from "./Denom";
 import "./App.css";
 
 class App extends Component {
@@ -7,9 +8,22 @@ class App extends Component {
     return (
       <div className="App" jsx="true">
         <header className="App-header">
+          <h3>Change Calculator</h3>
           <p>
-            <input name="amount" className="amount" />
+            <input
+              name="amount"
+              className="amount"
+              placeholder="amount"
+              type="number"
+              min="0"
+            />
           </p>
+          <Denom label="50" amount={2} />
+          <Denom label="0.5" amount={6} />
+          <Denom label="0.5" amount={6} />
+          <Denom label="0.5" amount={6} />
+          <Denom label="0.5" amount={6} />
+          <Denom label="0.5" amount={6} />
         </header>
       </div>
     );
